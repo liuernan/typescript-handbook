7833a01f783c909d6b5c9e9a0ccf0cbe23dbf013
liuernan <liujiaweidreamer@gmail.com>
2021-12-14 10:53:16 +0800

    function: parameter and return type annotations

--- 05-function.ts
+++ 05-function.ts
@@ -1,5 +1,12 @@
 #!/usr/bin/env ts-node
 
+import { argv } from "process";
+
 {
-  console.log(process);
+  const add = (a: number, b: number): number => {
+    console.log(a + b);
+    return a + b;
+  };
+
+  add(Number(argv[2]), Number(argv[3]));
 }
