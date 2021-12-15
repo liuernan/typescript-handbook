dad333b7a30df5f8793b65017ff4411d0c3ef1c6
liuernan <liujiaweidreamer@gmail.com>
2021-12-15 14:52:14 +0800

    buttons addEventListener

--- 07-calculator/07-calculator.js
+++ 07-calculator/07-calculator.js
@@ -8,9 +8,11 @@
                 ['1', '2', '3', '+'],
                 ['0', '.', '='],
             ];
+            this.result = '0';
             this.createContainer();
             this.createOutput();
             this.createOpertaion();
+            this.bindEvents();
         }
         Calculator.prototype.createContainer = function () {
             var $calc = document.createElement('div');
@@ -21,25 +23,35 @@
         Calculator.prototype.createOutput = function () {
             var $output = document.createElement('div');
             $output.classList.add('calc-output');
+            this.$result = document.createElement('span');
+            this.$result.textContent = this.result;
+            $output.appendChild(this.$result);
             this.$container.appendChild($output);
         };
         Calculator.prototype.createOpertaion = function () {
             var _this = this;
-            var $oper = document.createElement('div');
-            $oper.classList.add('calc-oper');
-            this.$container.appendChild($oper);
+            this.$oper = document.createElement('div');
+            this.$oper.classList.add('calc-oper');
             this.buttonList.forEach(function (arr) {
                 var container = document.createElement('div');
                 container.classList.add('calc-oper-row');
                 arr.forEach(function (item) { return _this.createButton(item, container); });
-                $oper.appendChild(container);
+                _this.$oper.appendChild(container);
             });
+            this.$container.appendChild(this.$oper);
         };
         Calculator.prototype.createButton = function (text, $el) {
             var button = document.createElement('button');
             button.innerText = text;
             button.className = "button-".concat(text);
             $el.appendChild(button);
+        };
+        Calculator.prototype.bindEvents = function () {
+            this.$oper.addEventListener('click', function (e) {
+                if (e.target instanceof HTMLButtonElement) {
+                    console.log(e.target.textContent);
+                }
+            });
         };
         return Calculator;
     }());
--- 07-calculator/07-calculator.ts
+++ 07-calculator/07-calculator.ts
@@ -1,6 +1,8 @@
 {
   class Calculator {
     $container: HTMLDivElement;
+    $oper: HTMLDivElement;
+    $result: HTMLSpanElement;
     buttonList: Array<Array<string>> = [
       ['clear', '÷'],
       ['7', '8', '9', '×'],
@@ -8,10 +10,14 @@
       ['1', '2', '3', '+'],
       ['0', '.', '='],
     ];
+    n1: string;
+    n2: string;
+    result: string = '0';
     constructor() {
       this.createContainer();
       this.createOutput();
       this.createOpertaion();
+      this.bindEvents();
     }
     createContainer(): void {
       const $calc = document.createElement('div');
@@ -22,25 +28,38 @@
     createOutput(): void {
       const $output = document.createElement('div');
       $output.classList.add('calc-output');
+
+      this.$result = document.createElement('span');
+      this.$result.textContent = this.result;
+      $output.appendChild(this.$result);
+
       this.$container.appendChild($output);
     }
     createOpertaion(): void {
-      const $oper = document.createElement('div');
-      $oper.classList.add('calc-oper');
-      this.$container.appendChild($oper);
+      this.$oper = document.createElement('div');
+      this.$oper.classList.add('calc-oper');
 
       this.buttonList.forEach((arr) => {
         const container = document.createElement('div');
         container.classList.add('calc-oper-row');
         arr.forEach((item) => this.createButton(item, container));
-        $oper.appendChild(container);
+        this.$oper.appendChild(container);
       });
+
+      this.$container.appendChild(this.$oper);
     }
     createButton(text: string, $el: HTMLElement): void {
       const button: HTMLButtonElement = document.createElement('button');
       button.innerText = text;
       button.className = `button-${text}`;
       $el.appendChild(button);
+    }
+    bindEvents(): void {
+      this.$oper.addEventListener('click', (e) => {
+        if (e.target instanceof HTMLButtonElement) {
+          console.log(e.target.textContent);
+        }
+      });
     }
   }
 
