5dd963fb5d2fe297912d90113b2864c02a07e6cf
liuernan <liujiaweidreamer@gmail.com>
2021-12-15 14:28:45 +0800

    calculator html & css finished

--- 07-calculator/07-calculator.js
+++ 07-calculator/07-calculator.js
@@ -1,3 +1,47 @@
 {
-    console.log(1);
+    var Calculator = /** @class */ (function () {
+        function Calculator() {
+            this.buttonList = [
+                ['clear', '÷'],
+                ['7', '8', '9', '×'],
+                ['4', '5', '6', '-'],
+                ['1', '2', '3', '+'],
+                ['0', '.', '='],
+            ];
+            this.createContainer();
+            this.createOutput();
+            this.createOpertaion();
+        }
+        Calculator.prototype.createContainer = function () {
+            var $calc = document.createElement('div');
+            $calc.classList.add('calc-container');
+            document.body.appendChild($calc);
+            this.$container = $calc;
+        };
+        Calculator.prototype.createOutput = function () {
+            var $output = document.createElement('div');
+            $output.classList.add('calc-output');
+            this.$container.appendChild($output);
+        };
+        Calculator.prototype.createOpertaion = function () {
+            var _this = this;
+            var $oper = document.createElement('div');
+            $oper.classList.add('calc-oper');
+            this.$container.appendChild($oper);
+            this.buttonList.forEach(function (arr) {
+                var container = document.createElement('div');
+                container.classList.add('calc-oper-row');
+                arr.forEach(function (item) { return _this.createButton(item, container); });
+                $oper.appendChild(container);
+            });
+        };
+        Calculator.prototype.createButton = function (text, $el) {
+            var button = document.createElement('button');
+            button.innerText = text;
+            button.className = "button-".concat(text);
+            $el.appendChild(button);
+        };
+        return Calculator;
+    }());
+    new Calculator();
 }
--- 07-calculator/07-calculator.ts
+++ 07-calculator/07-calculator.ts
@@ -1,3 +1,48 @@
 {
-  console.log(1);
+  class Calculator {
+    $container: HTMLDivElement;
+    buttonList: Array<Array<string>> = [
+      ['clear', '÷'],
+      ['7', '8', '9', '×'],
+      ['4', '5', '6', '-'],
+      ['1', '2', '3', '+'],
+      ['0', '.', '='],
+    ];
+    constructor() {
+      this.createContainer();
+      this.createOutput();
+      this.createOpertaion();
+    }
+    createContainer(): void {
+      const $calc = document.createElement('div');
+      $calc.classList.add('calc-container');
+      document.body.appendChild($calc);
+      this.$container = $calc;
+    }
+    createOutput(): void {
+      const $output = document.createElement('div');
+      $output.classList.add('calc-output');
+      this.$container.appendChild($output);
+    }
+    createOpertaion(): void {
+      const $oper = document.createElement('div');
+      $oper.classList.add('calc-oper');
+      this.$container.appendChild($oper);
+
+      this.buttonList.forEach((arr) => {
+        const container = document.createElement('div');
+        container.classList.add('calc-oper-row');
+        arr.forEach((item) => this.createButton(item, container));
+        $oper.appendChild(container);
+      });
+    }
+    createButton(text: string, $el: HTMLElement): void {
+      const button: HTMLButtonElement = document.createElement('button');
+      button.innerText = text;
+      button.className = `button-${text}`;
+      $el.appendChild(button);
+    }
+  }
+
+  new Calculator();
 }
