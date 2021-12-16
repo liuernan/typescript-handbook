07ac31c5f1e28a5880d62ad6b047249931c4059f
liuernan <liujiaweidreamer@gmail.com>
2021-12-16 18:09:23 +0800

    add 08-enum.ts

---
+++ 08-enum.ts
@@ -0,0 +1,12 @@
+{
+  enum Gender {
+    Male = 'Male',
+    Female = 'Female',
+  }
+
+  const male: Gender = Gender.Male;
+  console.log(male);
+
+  const female: Gender = Gender.Female;
+  console.log(female);
+}
