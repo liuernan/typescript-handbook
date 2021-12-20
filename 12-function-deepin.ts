2efc6cbf911c901acbdb23cc308276aa90d67577
liuernan <liujiaweidreamer@gmail.com>
2021-12-20 17:30:24 +0800

    function overloads

--- 12-function-deepin.ts
+++ 12-function-deepin.ts
@@ -7,4 +7,15 @@
   }
 
   console.log(`${typeof add(1, 2)}  ${add(1, 2)}`);
+
+  // overloads
+  function add2(n1: number, n2: number): number;
+  function add2(n1: string, n2: string): string;
+  function add2(n1: any, n2: any) {
+    return n1 + n2;
+  }
+
+  console.log(`${typeof add2(1, 2)}  ${add2(1, 2)}`);
+  console.log(`${typeof add2('1', '2')}  ${add2('1', '2')}`);
+  // add2(1, '2') // error
 }
